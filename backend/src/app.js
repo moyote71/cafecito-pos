3c4d022cf0a123f2450b924fcf23eaa2afd46cf9
moyote71 <moyote71@gmail.com>
2026-06-18 14:56:44 -0600

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 app.use(cors({
   origin: ["http://localhost:3000",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://cafecito-pos-zeta.vercel.app"
 ],
   credentials: true
   
